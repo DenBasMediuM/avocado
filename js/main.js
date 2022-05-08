be934df8d7e7b490b90be3fe1ae3517421f4ab1e
Денис Исаев <denisisaev@MacBook-Air-Denis.local>
2022-05-08 17:44:12 +0300

    final commit2

--- js/main.js
+++ js/main.js
@@ -234,3 +234,11 @@ $(document).ready(function(){
         // ]
     });
 });
+arrowTop.onclick = function() {
+    window.scrollTo(pageXOffset, 0);
+    // после scrollTo возникнет событие "scroll", так что стрелка автоматически скроется
+  };
+
+  window.addEventListener('scroll', function() {
+    arrowTop.hidden = (pageYOffset < document.documentElement.clientHeight);
+  });
